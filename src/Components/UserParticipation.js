e26d5f17691421133564ae6628a3a99df411a311
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-05-27 08:24:03 -0400

    Add App Component

--- src/Components/UserParticipation.js
+++ src/Components/UserParticipation.js
@@ -24,25 +24,7 @@ function UserParticipation({ choices, changeVotes, Q, fun }) {
             <label htmlFor={val.label}>{val.label}</label>
           </div>
         ))}
-        <button
-          onClick={() => {
-            let q = Q;
-            let { choices } = q;
-
-            choices.map((val) => {
-              console.log(val.label);
-              if (val.label === ans) {
-                console.log(ans);
-                val.votes = val.votes + 1;
-              }
-            });
-
-            changeVotes(q);
-            fun();
-          }}
-        >
-          Submit
-        </button>
+        <button onClick={() => {}}>Submit</button>
       </center>
     </div>
   );
